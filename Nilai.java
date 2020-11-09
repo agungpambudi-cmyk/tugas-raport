8e961edb710e6939679d13f7968ee39d364f6d26
agungpambudi-cmyk <hita56164@gmail.com>
2020-11-09 22:34:35 +0700

    projek banyak

---
+++ Nilai.java
@@ -0,0 +1,12 @@
+package com.Agung;
+
+public class Nilai {
+
+    public int getnilaiPengetahuan (int pengetahuan){
+        return pengetahuan;
+    }
+
+    public int getnilaiKeterampilan (int keterampilan){
+        return keterampilan;
+    }
+}
